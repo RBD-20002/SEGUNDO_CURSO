f0d7ec3be940800bb34c80fe51debc765c80fd5c
Ricardo <ricardo03-03-02@htomail.com>
2025-09-17 21:01:57 +0200

    avance en AD

--- AD/MySQL/repaso302.sql
+++ AD/MySQL/repaso302.sql
@@ -8,10 +8,17 @@ select clt_nome as nombre, clt_apelidos as apellidos, ven_data as fecha from cli
 select count(ven_id) as "total ventas", count(dev_venda) as "articulo vendidos", sum(dev_cantidade) as "unidades vendidas", avg(dev_prezo_unitario) as "media precio" from detalle_vendas join vendas on dev_numero = ven_id;
 
 /*4*/
-select art_codigo as numero, art_nome as nombre, art_peso as peso from artigos
+select art_codigo as numero, art_nome as nombre, art_peso as peso, (select peso_nome from pesos where art_peso BETWEEN peso_min and peso_max) AS peso_final from artigos;
 
 /*5*/
 
+/*A*/
+select dev_venta as "Cantidad Vendida", dev_numero as "Identificador", dev_artigo as "Nombre Articulo", ven_data as "Fecha Venta" from vendas join detalle_vendas on ven_id = dev_numero;
+/*B*/
+select 
+/*C*/
+
+/*D*/
 
 /*6*/
 
